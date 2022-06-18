37ddde7e27102a0aed2a479f168ba7243ecfba0b
Amine Bouaziz <amine.bouaziz@concircle.com>
2022-06-18 19:43:17 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    const getPosts = async () => {
+      const url = "https://jsonplaceholder.typicode.com/posts";
+      const resp = await fetch(url);
+      const body = await resp.json();
+    };
+  });
+  return <div className="App"></div>;
 }
 
 export default App;
